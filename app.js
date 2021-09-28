ad4a520fc66c0ced82204b6bdd6657c2676534cb
mohitdhatrak <msdhatrak25@gmail.com>
2021-09-29 02:28:14 +0530

    updated empty string error case

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ var outputText = document.querySelector("#output-text");
 var url = "https://api.funtranslations.com/translate/minion.json";
 
 function clickListner() {
-    var customUrl = url + "?text=" + inputText.value;
-    fetch(customUrl)
-        .then((resp) => resp.json())
-        .then((data) => {
-            if (data.error?.code == 429)
+    if (inputText.value === "") {
+        outputText.innerText = "Please enter some text.";
+    } else {
+        var customUrl = url + "?text=" + inputText.value;
+        fetch(customUrl)
+            .then((resp) => resp.json())
+            .then((data) => {
+                if (data.error?.code == 429)
+                    outputText.innerText =
+                        "You can only translate 5 times in 1 hour, please try after 1 hour";
+                else outputText.innerText = data.contents.translated;
+            })
+            .catch((error) => {
                 outputText.innerText =
-                    "You can only translate 5 times in 1 hour, please try after 1 hour";
-            else outputText.innerText = data.contents.translated;
-        })
-        .catch((error) => {
-            outputText.innerText =
-                "Some error occured, please try again later.";
-            console.log(error);
-        });
+                    "Some error occured, please try again later.";
+                console.log(error);
+            });
+    }
 }
 
 translateButton.addEventListener("click", clickListner);
